a3c3c9389060db1b54a822ccc96bb2d38066c052
slametsupriyadi.XJB2@gmail.com <slametsupriyadi.XJB2@gmail.com>
2024-06-10 21:22:26 +0700

    first commit

---
+++ application/views/global/home.php
@@ -0,0 +1,45 @@
+<div class="row">
+	<div class="col m 12">
+		<center>
+			<h5>Selamat Datang Di Sistem Informasi Perpustakaan Sekolah Islam Arrainsiyah</h5>
+		</center><hr>
+	</div>
+
+	<div class="row">
+    <div class="col m12">
+        <div class="card-panel teal darken-2 center white-text">
+            <p class="text-justify">
+                Selamat datang di Perpustakaan Sekolah Islam Arrainsiyah, sebuah pusat pembelajaran dan pengetahuan yang menyediakan akses ke koleksi yang luas dan beragam, serta fasilitas modern untuk siswa, guru, dan masyarakat umum. Terletak di pusat komunitas pendidikan Islam, kami berkomitmen untuk memfasilitasi pemahaman yang mendalam tentang nilai-nilai Islam serta menyediakan sumber daya pendidikan berkualitas dalam berbagai bidang studi. Bersama-sama, mari menjelajahi dan mendorong kecemerlangan pendidikan bagi masa depan yang lebih cerah. Selamat menikmati pengalaman literasi di Perpustakaan Sekolah Islam Arrainsiyah!
+            </p>
+        </div>
+    </div>
+</div>
+
+		</div>
+	</div>
+	<div class="row">
+		<div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/cc.jpg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/profile">TENTANG PERPUSTAKAAN</a>
+				</div>
+			</div>
+		</div>
+
+        <div class="col m6 s12">
+			<div class="card">
+				<div class="card-image waves-effect waves-block waves-light">
+					<img src="<?php echo base_url(); ?>assets2/img/aa2.jpeg"> <!-- random image -->
+				</div>
+				<div class="card-content teal center">
+                    <a class="btn" href="<?php echo base_url();?>Buku/list_buku">DAFTAR BUKU</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+</div>
---
+++ application/views/global/profile.php
@@ -0,0 +1,34 @@
+<div class="row">
+	<div class="col m 12">
+		<h5>Profil Perpustakaan Arrainsiyah</h5>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col m6">
+		<div class="card-panel teal darken-2 white-text">
+			<h6><b>Visi</b></h6>
+			<p>
+			Menjadi pusat pengetahuan dan inspirasi bagi siswa untuk mencapai keunggulan 
+			akademik dan karakter yang kuat dalam suasana yang menyenangkan dan bersemangat
+			</p><br>
+			<h6><b>Misi</b></h6>
+			<li>
+			Menyediakan koleksi bahan bacaan yang bermutu dan beragam sesuai dengan kebutuhan dan minat siswa.			</li>
+			<li>
+			Mendorong minat membaca dan literasi siswa melalui program-program kreatif dan edukatif.
+			</li>
+			<li>
+			Mendukung pembelajaran di kelas dengan menyediakan sumber daya informasi yang relevan dan terkini.			</li>
+			<li>
+			Menjadi tempat yang nyaman dan ramah bagi siswa untuk belajar, berdiskusi, dan mengeksplorasi dunia pengetahuan.			</li>
+		</div>
+	</div>
+    <div class="col m6">
+        <div class="card-panel teal darken-2 white-text">
+            <h6><b>Identitas Perpustakaan</b></h6><hr>
+            <p>
+			Perpustakaan Sekolah Dasar Islam Arrainsyah adalah sebuah fasilitas pendidikan di bawah naungan Sekolah Dasar Islam Arrainsyah yang bertujuan untuk mendukung pembelajaran dan pengembangan literasi siswa. Perpustakaan ini menyediakan akses ke berbagai jenis bahan bacaan yang relevan dengan kurikulum sekolah dan kebutuhan pembelajaran siswa. Dengan koleksi yang beragam dan suasana yang nyaman, perpustakaan bertujuan untuk menjadi tempat yang menginspirasi siswa untuk mengeksplorasi pengetahuan, membaca, dan mengembangkan minat literasi mereka            </p>
+        </div>
+    </div>
+</div>
